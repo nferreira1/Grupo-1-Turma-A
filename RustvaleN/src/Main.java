4ce9080afe1ebbbe1913761dab656ae89f9f635f
MuriloVieira49 <Murilo.vieira49@hotmail.com>
2023-06-15 23:39:11 -0300

    Implementando dificuldade no jogo

--- RustvaleN/src/Main.java
+++ RustvaleN/src/Main.java
@@ -36,38 +36,66 @@ public class Main {
 
                 switch (iniciar) {
                     case 1:
-System.out.println("Era uma vez, em um reino chamado RustvaleN, um lugar repleto de magia e aventuras. Por séculos, RustvaleN prosperou em paz,\n" +
-"até que um mal ancestral despertou nas profundezas do subterrâneo. Escuridão e caos começaram a se espalhar, ameaçando engolir o reino em trevas.\n" +
-"Aria, Leon e Maya, três heróis valentes e destinados, foram chamados pelos deuses para enfrentar a terrível ameaça que pairava sobre RustvaleN.\n" +
-"Cada um dos heróis possuía habilidades únicas e diferentes fraquezas e pontos fortes.\n\n"
-);
+                        System.out.println("Era uma vez, em um reino chamado RustvaleN, um lugar repleto de magia e aventuras. Por séculos, RustvaleN prosperou em paz,\n" +
+                                "até que um mal ancestral despertou nas profundezas do subterrâneo. Escuridão e caos começaram a se espalhar, ameaçando engolir o reino em trevas.\n" +
+                                "Aria, Leon e Maya, três heróis valentes e destinados, foram chamados pelos deuses para enfrentar a terrível ameaça que pairava sobre RustvaleN.\n" +
+                                "Cada um dos heróis possuía habilidades únicas e diferentes fraquezas e pontos fortes.\n\n"
+                        );
 
 // VIDA DO PERSONAGEM
-short vidaPersonagem = 0;
+                        short vidaPersonagem = 0;
 
 // ESCOLHA DA DIFICULDADE DO JOGO
-byte dificuldade = 0;
-do {
-try {
-System.out.println("Escolha a dificuldade: ");
-System.out.println("1 - Fácil\n" +
-"2 - Normal\n" +
-"3 - Difícil\n"
-);
-dificuldade = sc.nextByte();
-} catch (InputMismatchException exception) {
-System.out.println("Caracteres não permitido, insira apenas números.");
-sc.nextLine();
-}
-} while (dificuldade < 1 || dificuldade > 3);
+                        byte dificuldade = 0;
+                        do {
+                            try {
+                                System.out.println("Escolha a dificuldade: ");
+                                System.out.println("1 - Fácil\n" +
+                                        "2 - Normal\n" +
+                                        "3 - Difícil\n"
+                                );
+                                dificuldade = sc.nextByte();
+                            } catch (InputMismatchException exception) {
+                                System.out.println("Caracteres não permitido, insira apenas números.");
+                                sc.nextLine();
+                            }
+                        } while (dificuldade < 1 || dificuldade > 3);
 
-if (dificuldade == 1) {
-vidaPersonagem = 999;
-} else if (dificuldade == 2) {
-vidaPersonagem = 100;
-} else {
-vidaPersonagem = 1;
-}
+                        if (dificuldade == 1) {
+                            vidaPersonagem = 999;
+                        } else if (dificuldade == 2) {
+                            vidaPersonagem = 100;
+                        } else {
+                            vidaPersonagem = 1;
+                        }
+
+                        // ESCOLHA DO PERSONAGEM
+                        byte personagem = 0;
+                        do {
+                            try {
+                                System.out.println("Escolha um personagem: ");
+                                System.out.println("1 - Aria\n" +
+                                        "2 - Leon\n" +
+                                        "3 - Maya\n"
+                                );
+                                personagem = sc.nextByte();
+                            } catch (InputMismatchException exception) {
+                                System.out.println("Caracteres não permitido, insira apenas números.");
+                                sc.nextLine();
+                            }
+
+                        } while (personagem < 1 || personagem > 3);
+
+                        String personagemString;
+
+                        if (personagem == 1) {
+                            personagemString = "Aria";
+                        } else if (personagem == 2) {
+                            personagemString = "Leon";
+                        } else {
+                            personagemString = "Maya";
+                        }
+
                         break;
                     case 2:
                         break;
