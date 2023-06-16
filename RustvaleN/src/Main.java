1420726746cf59f3311c85d479c7240a8741f53e
Nathan Ferreira <nathan.1402@hotmail.com>
2023-06-15 23:43:01 -0300

    Inserindo os desafios de cada chefe

--- RustvaleN/src/Main.java
+++ RustvaleN/src/Main.java
@@ -36,16 +36,42 @@ public class Main {
 
                 switch (iniciar) {
                     case 1:
+                        // DESAFIOS DILSONS, CHEFE DO PÂNTANO
+                        String[] perguntasDilsons = {
+                                "Quantas vezes o loop for executa no código a seguir?\n\nfor (int i = 0; i < 5; i++) {}",
+                                "Quantas vezes o loop while executa no código a seguir?\n\nint i = 0;\nwhile (i < 3) {\ni++;\n}"
+                        };
+
+                        // DESAFIOS REDIAS, O LORDE DAS SOMBRAS
+                        String[] perguntasRedias = {
+                                "Qual a saída do código a seguir?\n\nint[] array = {1, 2, 3, 4, 5};\nfor (int i = 0; i < array.length; i++) {\n    if (array[i] % 2 == 0) {\n        System.out.print(array[i] + \\\" \\\");\n    }\n}\"",
+                                "\"Qual a saída do código a seguir?\n\nint[] array = {1, 2, 3, 4, 5};\nfor (int i = array.length - 1; i >= 0; i--) {\n    System.out.print(array[i] + \\\" \\\");\n}\""
+                        };
+
+                        String[] respostasRedias = {
+                                "a) 2, 4 \nb) 1, 3, 5 \nc) 1, 2, 3, 4, 5 \nd) 1, 5\n",
+                                "a) 2, 4 \nb) 1, 3, 5 \nc) 1, 2, 3, 4, 5 \nd) 5, 4, 3, 2, 1\n",
+                        };
+
+                        // DESAFIO CLARUS, O DRAGÃO SOMBRIO
+                        String[] perguntasClarus = {
+                                "Qual a saída do código a seguir?\n\nint[] array = {1, 2, 3, 4, 5};\nint contador = 0;\nfor (int i = 0; i < array.length; i++) {\n    if (array[i] > 2) {\n        contador++;\n    }\n}\nSystem.out.println(contador);",
+                                "Qual a saída do código a seguir?\n\nint[] array = {1, 2, 3, 4, 5};\nint soma = 0;\nfor (int i = 0; i < array.length; i++) {\n    if (array[i] % 2 == 0) {\n        soma += array[i];\n    }\n}\nSystem.out.println(soma);"
+                        };
+                        String[] respostasClarus = {
+                                "a) 2, 4 \nb) 1, 3, 5 \nc) 3\nd) 1, 5, 4\n",
+                                "a) 5 \nb) 6 \nc) 2 \nd) 16\n",
+                        };
                         System.out.println("Era uma vez, em um reino chamado RustvaleN, um lugar repleto de magia e aventuras. Por séculos, RustvaleN prosperou em paz,\n" +
                                 "até que um mal ancestral despertou nas profundezas do subterrâneo. Escuridão e caos começaram a se espalhar, ameaçando engolir o reino em trevas.\n" +
                                 "Aria, Leon e Maya, três heróis valentes e destinados, foram chamados pelos deuses para enfrentar a terrível ameaça que pairava sobre RustvaleN.\n" +
                                 "Cada um dos heróis possuía habilidades únicas e diferentes fraquezas e pontos fortes.\n\n"
                         );
 
-// VIDA DO PERSONAGEM
+                        // VIDA DO PERSONAGEM
                         short vidaPersonagem = 0;
 
-// ESCOLHA DA DIFICULDADE DO JOGO
+                        // ESCOLHA DA DIFICULDADE DO JOGO
                         byte dificuldade = 0;
                         do {
                             try {
